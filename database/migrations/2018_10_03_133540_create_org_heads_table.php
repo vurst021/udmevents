a33c7635f54376bf7652019c3098c90bdbfca31d
leon <mrkdeasis@gmail.com>
2018-10-03 22:23:53 +0800

    updated models and migration

---
+++ database/migrations/2018_10_03_133540_create_org_heads_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrgHeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('org_heads', function (Blueprint $table) {
+            $table->increments('org_head_id');
+            $table->integer('org_head_userID')->unsigned()->nullable();
+            $table->foreign('org_head_userID')->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->integer('org_head_organizationID')->unsigned()->nullable();
+            $table->foreign('org_head_organizationID')->references('org_id')
+                  ->on('organizations')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('org_heads');
+    }
+}
